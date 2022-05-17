a36f48198775d26bf55759f2ed330383daecee0d
Tony Han <tony3641@126.com>
2022-05-17 17:33:29 +0800

    update content

--- my_quant_util.py
+++ my_quant_util.py
@@ -1,19 +1,39 @@
-from cmath import sqrt
-from pickle import FALSE
-from statistics import variance
-from symtable import Symbol
-from matplotlib import pyplot as plt
 import numpy as np
 from scipy.stats import norm
+import datetime
+from datetime import date
 
+
+class Stock:
+    has_position=False
+    def __init__(self, symbol, owned=False, position=None, cost=None, profit=None):
+        self.symbol=symbol
+        if owned is True:
+            self.position=position
+            self.cost=cost
+            self.profit=profit
+            self.has_position=True
 
 class Option:
     __greek_keys__=['delta','gamma','theta','vega','rho','impVol']
-    initialized= FALSE
+    initialized=False
+    has_position=False
+    def __calc_days_to_expire__(self):
+        now=datetime.datetime.now().strftime('%Y-%m-%d')
+        today=date(int(now[0:4]), int(now[5:7]), int(now[8:10]))
+        expire_date=date(int(self.expire[0:4]),int(self.expire[5:7]),\
+            int(self.expire[8:10]))
+        return (expire_date-today).days
 
-    def __init__(self, stock_symbol, id):
+    def __init__(self, stock_symbol, id, owned=False, position=None, cost=None, profit=None, market_value=None):
         self.symbol=stock_symbol
         self.option_id=id
+        if(owned):
+            self.position=position
+            self.cost=cost
+            self.profit=profit
+            self.market_value=market_value
+            self.has_positioin=True
 
     def update_info(self, webull):
         quote=webull.get_option_quote(stock=self.symbol,optionId=self.option_id)
@@ -25,7 +45,7 @@ class Option:
         self.bid_price=float(data['bidList'][0]['price'])
         self.greeks={greek_key: data[greek_key] for greek_key in self.__greek_keys__}
         self.name=data['symbol']
-
+        self.days_to_expire=self.__calc_days_to_expire__()
         self.initialized=True
 
     def display(self):
@@ -36,6 +56,23 @@ class Option:
             raise ValueError('Warning: option info not updated yet, \
                 please call Option.update_info().')
 
+    def display_position(self):
+        if (self.initialized is True) and (self.has_positioin is True):
+            print(self.name+'\t'+'position='+str(self.position)+'\t'+'avg cost='+str(self.cost)\
+                +'\t'+'market value='+str(self.market_value*self.position)+'\t'+'total profit=' \
+                +str(self.profit*self.position))
+        elif self.has_positioin is True:
+            print('Warning: You don\'t have any position.')
+        else:
+            raise ValueError('Warning: option info not updated yet, \
+                please call Option.update_info().')
+
+def get_stock_positions(positions,webull=None):
+    stock_positions=[]
+    for position in stock_positions:
+        if position['assetType']=='STOCK':
+            ticker=position['ticker']
+            
 def get_option_positions(positions,webull=None):
     option_positions=[]
     for position in positions:
@@ -43,7 +80,12 @@ def get_option_positions(positions,webull=None):
             ticker=position['ticker']
             symbol=ticker['symbol']
             option_id=position['tickerId']
-            option=Option(symbol,option_id)
+            position_owned=float(position['position'])
+            avg_cost=float(position['cost'])/position_owned
+            avg_profit=float(position['unrealizedProfitLoss'])/position_owned
+            value=float(position['marketValue'])/position_owned
+            option=Option(stock_symbol=symbol,id=option_id,owned=True,position=position_owned,\
+                cost=avg_cost,profit=avg_profit,market_value=value)
             if webull is not None:
                 option.update_info(webull)
             option_positions.append(option)
@@ -122,3 +164,10 @@ def calc_stock_volatility(period_close_price):
         variance+=((r[i]-r_avg)**2)/(len(r)-1)
     std_dev=np.sqrt(252)*np.sqrt(variance) #annualized sd with 252 trading days per year
     return std_dev
+
+"""Automatically inquires daily close of the underlying stock via Webull API"""
+def calc_stock_volatility_auto_quote(webull, stock, days):
+    stock_quote=webull.get_bars(stock=stock,interval='d1',count=days)['close'].tolist()
+    vol=calc_stock_volatility(stock_quote)
+    return vol
+
--- wb_login.py
+++ wb_login.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import flag
 from webull import webull
 import my_quant_util
 import numpy as np
@@ -6,6 +7,9 @@ import getpass
 
 wb=webull()
 loggedin=False
+cmd=''
+local_order_id=0
+order={}
 usr_id=input('Enter username:')
 while(not loggedin):
     passwd=getpass.getpass('Enter password:')
@@ -21,16 +25,47 @@ trade_token_authed=False
 while(not trade_token_authed):
     trade_token=getpass.getpass('Enter 6-digit trade token:')
     trade_token_authed=wb.get_trade_token(trade_token)
-print('Trade token autherized. Ready to order now\n\n')
-
+print('Trade token autherized. Ready to order now\n')
 positions=wb.get_positions()
+print('--------------  Your stock positions  -------------')
+print('\n')
 option_positions=my_quant_util.get_option_positions(positions,wb)
+print('-------------  Your option positions  -------------')
 for option_position in option_positions:
     option_position.display_position()
+print('---------------------------------------------------')
+while(True):
+    cmd=input('quant_sys$:') #COMMAND TICKER QUANT PRICE
+    if cmd == 'q!' :
+        break
+    else:
+        flags=cmd.split()
 
-while(input() != 'q!'):
-    print('Enter next command.')
-
+    if flags[0]=='BUYSTOCKLMT':
+        ret=wb.place_order(stock=flags[1],quant=int(flags[2]),price=int(flags[3]))
+        if(ret['success']==True):
+            order.update({local_order_id:{'remoteId':ret['data']['orderId'],\
+                'stock':flags[1],'quantity':flags[2],'price':flags[3],'type':'BUYLMT'}})
+            print('order success, local order id='+str(local_order_id))
+            local_order_id+=1
+    elif flags[0]=='BUYSTOCKMKT':
+        wb.place_order(stock=flags[1],quant=int(flags[2]),orderType='MKT')
+        if(ret['success']==True):
+            order.update({local_order_id:ret['data']['orderId']})
+            print('order success, local order id='+str(local_order_id))
+            local_order_id+=1
+    elif flags[0]=='CANCEL':
+        if flags[1]=='ALL':
+            wb.cancel_all_orders()
+            print('All orders canceled.')
+        else:
+            ret=wb.cancel_order(order_id=order[int(flags[1])]['remoteId'])
+            if(ret==True):
+                print(str(order[int(flags[1])]['type'])+' '+str(order[int(flags[1])]['stock'])\
+                    +' '+str(order[int(flags[1])]['quantity'])+' '+str(order[int(flags[1])]['price'])\
+                    +' canceled.')
+    else:
+        print('Error.')
 
 if(wb.logout()>=0):
     print('Logged out.')
