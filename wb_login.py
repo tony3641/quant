4713b8d571e81aeaf8d9a3250a9408d12c1217e9
Tony Han <tony3641@126.com>
2022-05-17 16:30:36 +0800

    Update lib

--- wb_login.py
+++ wb_login.py
@@ -1,5 +1,5 @@
 from webull import webull
-import option_util
+import my_quant_util
 import numpy as np
 import pandas as pd
 import getpass
@@ -9,15 +9,28 @@ loggedin=False
 usr_id=input('Enter username:')
 while(not loggedin):
     passwd=getpass.getpass('Enter password:')
-    print("---Autheticating, this might take a while---")
+    print('\n---Autheticating, this might take a while---\n')
     login_status=wb.login(username=usr_id,password=passwd)
     if 'accessToken' in login_status:
-        print('Login successfully.')
+        print('Login successfully.\n')
         loggedin=True
     else:
         print(login_status['msg'])
 
-trade_token=getpass.getpass('Enter 6-digit trade token:')
-wb.get_trade_token(trade_token)
+trade_token_authed=False
+while(not trade_token_authed):
+    trade_token=getpass.getpass('Enter 6-digit trade token:')
+    trade_token_authed=wb.get_trade_token(trade_token)
+print('Trade token autherized. Ready to order now\n\n')
 
-#wb.logout()
+positions=wb.get_positions()
+option_positions=my_quant_util.get_option_positions(positions,wb)
+for option_position in option_positions:
+    option_position.display_position()
+
+while(input() != 'q!'):
+    print('Enter next command.')
+
+
+if(wb.logout()>=0):
+    print('Logged out.')
